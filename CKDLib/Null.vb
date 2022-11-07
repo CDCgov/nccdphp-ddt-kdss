446a89cdd9d6d35e83d1debb3571a34c7d8e4cdd
Belov, Alex (CDC/DDNID/NCCDPHP/OD) (CTR) <zxo7@cdc.gov>
2022-11-07 12:00:32 +0000

    Upload New File

---
+++ CKDLib/Null.vb
@@ -0,0 +1,16 @@
+﻿    Public Class Null
+        Public Shared Function StringtoNull(ByVal strValue As String) As Object
+            If strValue = String.Empty Then
+                Return DBNull.Value
+            Else
+                Return strValue
+            End If
+        End Function
+        Public Shared Function InttoNull(ByVal intValue As Integer) As Object
+            If intValue = 0 Then
+                Return DBNull.Value
+            Else
+                Return intValue
+            End If
+        End Function
+    End Class
