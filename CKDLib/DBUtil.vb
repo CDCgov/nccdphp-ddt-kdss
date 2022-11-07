038ae89ecf89b17be27211712b9a8bed4497a690
Belov, Alex (CDC/DDNID/NCCDPHP/OD) (CTR) <zxo7@cdc.gov>
2022-11-07 11:59:36 +0000

    Upload New File

---
+++ CKDLib/DBUtil.vb
@@ -0,0 +1,21 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Data
+Imports System.Web
+Imports System.Text
+Imports System.Data.SqlClient
+Imports System.Data.Common
+Imports System.Configuration
+Imports NG.NCCDPHP.DataAccess
+Imports NG.NCCDPHP.Utilities
+Imports NG.NCCDPHP.DataAccess.Sql
+
+Public Class DBUtil
+    Public Shared Function GetSqlDBConnection() As SqlDatabase
+        Dim sqlDB As New SqlDatabase(ConfigurationManager.AppSettings("Database"), _
+                                     ConfigurationManager.AppSettings("DBAccount"), _
+                                     ConfigurationManager.AppSettings("DBServer"), _
+                                     ConfigurationManager.AppSettings("DBKey"))
+        Return sqlDB
+    End Function
+End Class
