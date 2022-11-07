587a739191839dbc8b4c62b4ff0ea59ee4a1059d
Belov, Alex (CDC/DDNID/NCCDPHP/OD) (CTR) <zxo7@cdc.gov>
2022-11-07 11:59:13 +0000

    Upload New File

---
+++ CKDLib/DBCONSTANTS.vb
@@ -0,0 +1,14 @@
+﻿Public Class DBCONSTANTS
+    'Database Columns
+    Public Shared SURVEY As String = "Survey"
+    Public Shared SURVEYINSTANCE As String = "SurveyInstance"
+    Public Shared STRATIFICATION As String = "Stratification"
+    Public Shared STRATIFICATIONTYPE As String = "StratificationType"
+    Public Shared CHOICE As String = "Choice"
+    Public Shared QUESTION As String = "Question"
+    Public Shared DATAPOINT As String = "Datapoint"
+    Public Shared METADATA As String = "Metadata"
+    Public Shared FOOTNOTE As String = "Footnote"
+    Public Shared CONNECTIONSTRING As String = "sConnectionString"
+
+End Class
