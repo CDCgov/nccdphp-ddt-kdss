420a9e17f8723ce76d5b8772731d2abe46f7831d
Belov, Alex (CDC/DDNID/NCCDPHP/OD) (CTR) <zxo7@cdc.gov>
2022-11-07 12:01:19 +0000

    Upload New File

---
+++ CKDLib/Utilities.vb
@@ -0,0 +1,33 @@
+﻿Public Class Utilities
+
+    Public Shared Function CheckForNumeric(ByVal val As String) As Boolean
+        Dim answer = False
+
+        If IsNumeric(val) Then
+            answer = True
+        End If
+
+        Return answer
+    End Function
+    Public Shared Function CheckForDBNull(ByVal val As String) As Boolean
+        Dim answer = False
+
+        If IsDBNull(val) Then
+            answer = True
+        End If
+
+        Return answer
+    End Function
+
+    Public Shared Function CheckIfValueExists(ByVal val As String) As Boolean
+        Dim answer As Boolean = True
+
+        If String.IsNullOrEmpty(val) OrElse String.IsNullOrWhiteSpace(val) Then
+            answer = False
+        ElseIf Not val Is Nothing AndAlso val <> "" AndAlso val.Length > 0 Then
+            answer = True
+        End If
+
+        Return answer
+    End Function
+End Class
