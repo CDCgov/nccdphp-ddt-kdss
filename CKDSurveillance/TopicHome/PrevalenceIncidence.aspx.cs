54f2a82ce520067afd858510b37ebe25e5329ca8
Le, Thach <lez9@cdc.gov>
2025-08-26 09:40:17 -0400

    update dmat page name for main nav pages

--- CKDSurveillance/TopicHome/PrevalenceIncidence.aspx.cs
+++ CKDSurveillance/TopicHome/PrevalenceIncidence.aspx.cs
@@ -11,7 +11,7 @@ namespace CKDSurveillance_RD.TopicHome
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            Page.Title = "KDSS | Prevalence and Incidence | CDC";
         }
     }
 }
