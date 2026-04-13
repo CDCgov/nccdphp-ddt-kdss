910196d2cd56c033ce7a259c45bd7f74cfdefc15
Jun Park-Lee <JPark-Lee@mathematica-mpr.com>
2026-04-13 11:11:19 -0400

    Revert "Merge pull request #56 from CDCgov/2026AprRelease-1"
    This reverts commit be2e25f699e88907496d5b14f60a52686f860570, reversing
    changes made to ae23758b6819d679cec0d96889f2156c712aabc0.

--- CKDSurveillance/detail.aspx.designer.cs
+++ CKDSurveillance/detail.aspx.designer.cs
@@ -105,15 +105,6 @@ namespace CKDSurveillance_RD.MasterPages
         protected global::System.Web.UI.WebControls.HiddenField hfShowCI;
 
         /// <summary>
-        /// hfShowCIShadow control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HiddenField hfShowCIShadow;
-
-        /// <summary>
         /// hfMapType control.
         /// </summary>
         /// <remarks>
