1397c084fbbd4707723b0e5c8fb2aa158a7f9f31
Maxwell Miller <MEMiller@mathematica-mpr.com>
2026-05-27 13:50:42 -0400

    Updates based on cleared version of the AYA

--- CKDDatabase/DEV/20260601-2026June01Release/20260601-2026June01Release.Data.sql
+++ CKDDatabase/DEV/20260601-2026June01Release/20260601-2026June01Release.Data.sql
@@ -18,14 +18,14 @@ INSERT INTO dbo.AYA
 VALUES
 (   N'June',  -- Month - nchar(10)
     '2026',   -- Year - varchar(4)
-    N'Chronic Kidney Disease in the United States: The New CDC Fact Sheet is Published',  -- Title - nvarchar(2000)
+    N'Chronic Kidney Disease in the United States: The New CDC Report on the State of CKD',  -- Title - nvarchar(2000)
     N'../AreYouAware.aspx?emailDate=June_2026',  -- AYALink - nvarchar(200)
     '',   -- QNUM - varchar(6)
     N'',  -- CKDSurveillanceChartLink - nvarchar(200)
     N'',  -- ChartSpecifics - nvarchar(200)
-    'The CDC recently published its 2026 Factsheet on Chronic Kidney Disease (CKD). The Factsheet outlines key statistics and an overview of CKD Epidemiology, and overall management strategies.', -- ShortDescription - nvarchar(max)
-    NULL, -- TopicID - int
+    'CDC recently published its 2026 report on chronic kidney disease (CKD). The report shares updated national estimates of CKD in the United States. It also highlights how CKD affects different groups and shares steps people can take to help manage CKD and protect their kidney and heart health.', -- ShortDescription - nvarchar(max)
+    1, -- TopicID - int
     1, -- IsActive - bit
-    NULL, -- TopicHomePageURL - nvarchar(max)
+    '../TopicHome/PrevalenceIncidence.aspx?topic=1', -- TopicHomePageURL - nvarchar(max)
     5  -- MonthNum - int
     )
