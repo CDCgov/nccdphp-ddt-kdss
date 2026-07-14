9e41527c25716cee6faf8c9399f5ad687349ff8d
Maxwell Miller <MEMiller@mathematica-mpr.com>
2026-07-14 16:14:28 -0400

    Update db script with more specific link

--- CKDDatabase/DEV/20260715-2026July15Release/20260715-2026July15Release.sql
+++ CKDDatabase/DEV/20260715-2026July15Release/20260715-2026July15Release.sql
@@ -21,7 +21,7 @@ VALUES
     N'Increasing Use of GLP-1 RA Medications Among Adults With Type 2 Diabetes and Chronic Kidney Disease',  -- Title - nvarchar(2000)
     N'../AreYouAware.aspx?emailDate=August_2026',  -- AYALink - nvarchar(200)
     'Q813',   -- QNUM - varchar(6)
-    N'https://nccd.cdc.gov/CKD/detail.aspx?Qnum=Q813',  -- CKDSurveillanceChartLink - nvarchar(200)
+    N'https://nccd.cdc.gov/CKD/detail.aspx?Qnum=Q813&topic=1&Strat=CKD+Stage#refreshPosition',  -- CKDSurveillanceChartLink - nvarchar(200)
     N'',  -- ChartSpecifics - nvarchar(200)
     'Glucagon-like peptide-1 receptor agonists (GLP-1 RAs) have beneficial effects in the management of chronic kidney disease (CKD), type 2 diabetes, obesity, and cardiovascular disease. From 2016 to 2023, GLP-1 RA medication use increased among Military Health System beneficiaries with type 2 diabetes and CKD, rising from 5.5% to 36.6% overall. The rapidly increasing use of GLP-1 RAs shows a change in how type 2 diabetes and CKD are managed.', -- ShortDescription - nvarchar(max)
     6, -- TopicID - int
